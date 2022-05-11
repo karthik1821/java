b8454a3fe39790ddf51f9ee0bf9bb268aa1ba7aa
karthik1821 <105377969+karthik1821@users.noreply.github.com>
2022-05-11 21:52:59 +0530

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -1,14 +1,14 @@
-//I did in leetcode
+//leetcode 15
 class Solution {
     public List<List<Integer>> threeSum(int[] num) {
-        Arrays.sort(num);
+        Arrays.sort(num);// sort as tc is n^3 in bruteforce
     List<List<Integer>> res = new LinkedList<>(); 
     for (int i = 0; i < num.length-2; i++) {
         if (i == 0 || (i > 0 && num[i] != num[i-1])) {
             int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-            while (lo < hi) {
+            while (lo < hi) {//binary ssearch
                 if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                    res.add(Arrays.asList(num[i], num[lo], num[hi]));//add to result
                     while (lo < hi && num[lo] == num[lo+1]) lo++;
                     while (lo < hi && num[hi] == num[hi-1]) hi--;
                     lo++; hi--;
